66a37b407f12cdd3d025ee43b20632cf6597cfd5
Claudiney81 <149173560+Claudiney81@users.noreply.github.com>
2025-06-25 11:49:21 -0300

    Deploy inicial para Render

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
-def app():
-    from app import create_app
-    return create_app()
+import os
+from app import create_app
 
+app = create_app()
 
+# Cria as tabelas automaticamente no Railway
+with app.app_context():
+    from app.extensions import db
+    db.create_all()
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
 
